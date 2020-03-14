30db195afc6709d90f1dcbbbbcf8d32fc2bdb9bc
Emery Tate <tateejt@gmail.com>
2020-03-14 00:50:12 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -7,6 +7,11 @@ function changeState(state, action){
   }
 }
 
+function dispatch(action){
+  state = changeState(state, action)
+  return state
+}
+
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
